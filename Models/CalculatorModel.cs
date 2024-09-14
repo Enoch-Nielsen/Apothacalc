edd633a0c152b8090ba96ca284075abb706a9f3f
Enoch-Nielsen <93846909+Enoch-Nielsen@users.noreply.github.com>
2024-09-14 12:36:52 -0600

    Updated UI, Pending

--- Models/CalculatorModel.cs
+++ Models/CalculatorModel.cs
@@ -5,14 +5,14 @@ namespace Apothacalc.Models;
 public static class Calculator
 {
     public const double DAY_SUPPLY_MIN = 1.0;
-    public const double DAY_SUPPLY_MAX = 2000.0;
-    
+    public const double DAY_SUPPLY_MAX = 10000.0;
+
     public const double DOSE_MIN = 0.001;
     public const double DOSE_MAX = 10000;
-    
+
     public const double PER_WEEK_MIN = 1;
     public const double PER_WEEK_MAX = 100;
-        
+
     /// <summary>
     /// Returns the calculated value of either the Quantity or Day Supply depending on the given input
     /// and mode.
